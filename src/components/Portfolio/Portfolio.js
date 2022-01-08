fdadff3ff41fe18f6996ed5612a79f9b19d3d737
Nadezhda Platonova <platonova.nadezhda.webdev@yandex.ru>
2022-01-08 20:53:40 +0300

    fix: change links in Portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,19 +7,19 @@ function Portfolio() {
             <nav>
                 <ul className="portfolio__list">
                     <li className="portfolio__item">
-                        <a href="https://github.com/NadinePlatonova/how-to-learn" className="portfolio__link" target="_blank" rel="noreferrer">
+                        <a href="https://nadineplatonova.github.io/how-to-learn" className="portfolio__link" target="_blank" rel="noreferrer">
                             <p className="portfolio__text">Статичный сайт</p>
                             <button className="portfolio__button"></button>
                         </a>
                     </li>
                     <li className="portfolio__item">
-                        <a href="https://nadineplatonova.github.io/russian-travel/" className="portfolio__link" target="_blank" rel="noreferrer">
+                        <a href="https://nadineplatonova.github.io/russian-travel" className="portfolio__link" target="_blank" rel="noreferrer">
                             <p className="portfolio__text">Адаптивный сайт</p>
                             <button className="portfolio__button"></button>
                         </a>
                     </li>
                     <li className="portfolio__item">
-                        <a href="https://platonova.mesto.nomoredomains.rocks/" className="portfolio__link" target="_blank" rel="noreferrer">
+                        <a href="https://platonova.mesto.nomoredomains.rocks" className="portfolio__link" target="_blank" rel="noreferrer">
                             <p className="portfolio__text">Одностраничное приложение</p>
                             <button className="portfolio__button"></button>
                         </a>
