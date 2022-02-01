21afa28114858e6954e78243e43ca4ea0441d1e0
Nadezhda Platonova <platonova.nadezhda.webdev@yandex.ru>
2022-02-01 20:37:17 +0300

    fix: fix Navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import icon_account from '../../images/icon_account.svg';
 
-function Navigation() {
+function Navigation(props) {
     const location = useLocation();
 
     return (
             <div className="navigation">
                 <div className="navigation__container">
                     <ul className="navigation__movies">
-                        <li><NavLink to="/movies" className={`'navigation__item' ${location.pathname='/' ? 'navigation__item_theme_dark' : ''}`}>Фильмы</NavLink></li>
-                        <li><NavLink to="/saved-movies" className={`'navigation__item navigation__item_type_normal' ${location.pathname='/' ? 'navigation__item_theme_dark' : ''}`}>Сохранённые фильмы</NavLink></li>
+                        <li><NavLink to="/movies" className={`navigation__item ${props.loggedIn && location.pathname === '/' ? 'navigation__item_theme_dark' : ''}`}>Фильмы</NavLink></li>
+                        <li><NavLink to="/saved-movies" className={`navigation__item navigation__item_type_normal ${props.loggedIn && location.pathname === '/' ? 'navigation__item_theme_dark' : ''}`}>Сохранённые фильмы</NavLink></li>
                     </ul>
                     <ul className="navigation__profile">
-                        <li><NavLink to="/profile" className={`'navigation__item' ${location.pathname='/' ? 'navigation__item_theme_dark' : ''}`}>Аккаунт</NavLink></li>
+                        <li><NavLink to="/profile" className={`navigation__item ${props.loggedIn && location.pathname === '/' ? 'navigation__item_theme_dark' : ''}`}>Аккаунт</NavLink></li>
                         <li><img className="navigation__account-icon" src={icon_account} alt="Иконка аккаунта"/></li>
                     </ul>
                 </div>
